35e503fc36b38e72e63f64922e1edf9c584f762d
gulbin-dev <gulbindev@gmail.com>
2026-04-10 16:48:49 +0800

    feat: added window resize listener

---
+++ app/hooks/useWindowSizeListener.ts
@@ -0,0 +1,30 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { ScrollTrigger } from "@utils/gsap/gsap";
+
+/** Custom hook to get window size when resizing the window  */
+export default function useWindowSizeListener() {
+  // Implementation for window size listener
+  const [resizeKey, setResizeKey] = useState(0);
+  useEffect(() => {
+    let timeoutId: NodeJS.Timeout;
+
+    const handleResize = () => {
+      clearTimeout(timeoutId);
+      // Only update state after user stops resizing for 200ms
+      timeoutId = setTimeout(() => {
+        ScrollTrigger.refresh(); // Refresh ScrollTrigger on window resize
+        setResizeKey((prev) => prev + 1);
+      }, 200);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
+  return resizeKey; // Return the resizeKey to trigger re-renders in components that use this hook
+}
