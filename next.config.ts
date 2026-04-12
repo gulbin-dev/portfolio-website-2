a255374389b90cb3fbee8845327d78565a023e6b
Joshua Glenn R. Gulbin <gulbindev@gmail.com>
2026-04-12 16:14:03 +0800

    chore: Dev branch (#40)
    * feat: added bug finder AI agent
    
    * refractor: crop webm file
    
    * refractor: all useWindowSizeListener() once then pass props to custom hooks for animation
    
    * fix: improve perfomance by changing the approach on drawing the image to the canvas
    
    * refractor: removed snaping effect on mobile screens
    
    * fix: fix header animation playhead
    
    * refractor: initialize useWindowSlizeListener once
    
    * refractor(card-skill): improve scoping on animation
    
    refractor(video): improve code base readability
    
    * refractor: minor changes
    
    * refractor: remove unwanted scroll-behavior style
    
    * fix: fix some styles
    
    * refractor: adjust isSmallScreen media query
    
    * refractor: added some assets
    
    * refractor: remove animations on mobile
    
    * refractor: simplify animation approach
    
    * refractor: centralize react-icon imports
    
    * chore: optimize images on /home-page/
    
    * chore: improve image optimizations

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    deviceSizes: [320, 330, 380, 420, 450, 480, 768, 1080, 1200, 1400],
+    imageSizes: [250, 280, 300, 370, 700, 800, 900, 1000, 1200, 1400],
     remotePatterns: [
       {
         protocol: "https",
@@ -11,6 +13,7 @@ const nextConfig: NextConfig = {
         pathname: "/home-page/**",
       },
     ],
+    minimumCacheTTL: 2678400,
   },
 };
 
